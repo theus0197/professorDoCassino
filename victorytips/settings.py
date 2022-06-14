1f7f1200e6f1f2879828642ab8bd8f973d7adf36
Matheus Farias <68437209+thisfarias@users.noreply.github.com>
2022-06-14 20:22:02 -0300

    change settings

--- victorytips/settings.py
+++ victorytips/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4+@a(n%_&3#ivj@fsvp#@xm!pgvv0+e)yfy@%78^cs@$0diz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
